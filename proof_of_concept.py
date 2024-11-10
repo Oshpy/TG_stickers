ed363c2b268e19cf6f22413413dd8e16b36a6967
Oshpy <oleksandr.shpylovyi@gmail.com>
2024-11-10 22:04:47 +0100

    dev/ Change Download Sets, instead of sending one sticker
    1. Fix environment, rename variable bot to client
    2. Create folder function
    3. Reading dictionary of read stickers from file, to avoid double downloading of sticker sets
    4. Fetching stickerset and downloading each sticker separatelly. Setting it up in folders and logging stickers in dictionary. Dumping stickers_dictionary for logging purposes.
    5. Adding __name__==__main__

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,13 +1,15 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from telethon import TelegramClient
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID
+import json
 import pandas as pd
 import os
 import time
 
-load_dotenv()
-api_id = os.getenv('BOT_API_ID')
-api_hash = os.getenv('BOT_API_HASH')
-
+load_dotenv(find_dotenv('credentials.env'))
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
 # Channels to scrape stickers from
 chanels = pd.read_json('chanels.json')['chanels']
 for chanel in chanels:
@@ -32,23 +34,56 @@ t_index = 0  # Tracker for the number of messages processed
 start_time = time.time()
 '''
 
+def create_folder(folder):
+    if os.path.isdir(folder):
+        pass
+    else:
+        os.makedirs(folder,exist_ok=True)
+
 # Initialize the client
-bot = TelegramClient(session='sticker_scraper',api_id=api_id,api_hash=api_hash)
+client = TelegramClient(session='sticker_scraper',api_id=api_id,api_hash=api_hash)
+main_folder = "stickers"
+create_folder(main_folder)
+
+try:
+    with open('scraped_stickers.json', 'r') as file:
+        stickers_dict = json.load(file)
+except:
+    stickers_dict = {}
+
 async def scrape_and_send_stickers():
     # Channels to scrape stickers from
     for chanel in chanels:
         name = chanel['name']
         min_id = chanel['min_id']
+        print(f"Started scraping chanel: {name}")
         try:
-            async for message in bot.iter_messages(entity=name, limit=100000, min_id=min_id):
+            async for message in client.iter_messages(entity=name, limit=10, min_id=min_id):
                 # Filter only sticker messages
                 if message.sticker:
-                    # Send the sticker to yourself
-                    await bot.send_file('me', message.media)
-                    print(f"Sticker {message.sticker} sent to 'me' from {chanel}")
+                    sticker_set = message.media.document.attributes[1].stickerset
+                    if str(sticker_set.id) not in stickers_dict.keys():
+                        sub_folder = os.path.join(main_folder,str(sticker_set.id))
+                        create_folder(sub_folder)
+                        
+                        # Fetch the sticker set
+                        sticker_set_input = InputStickerSetID(id=sticker_set.id, access_hash=sticker_set.access_hash)
+                        sticker_request = await client(GetStickerSetRequest(stickerset=sticker_set_input,hash=0))
+                        stickers_dict[str(sticker_set.id)] = [str(sticker.id) for sticker in sticker_request.documents]
+                        
+                        for sticker in sticker_request.documents:
+                            print(f"Downloading stickerset: {sticker_set.id}")
+                            await client.download_media(sticker, file=os.path.join(sub_folder,str(sticker.id)))
+        
         except Exception as e:
-            print(f"Error in channel {chanel}: {e}")
-    await bot.disconnect()
+            print(f"Error in channel {name}: {e}")
+        
+        print(f"Stoped scraping chanel: {name}")
+    await client.disconnect() 
+    
+    with open("scraped_stickers.json", "w") as f:
+        json.dump(stickers_dict, f, indent=4)
 
-with bot:
-    bot.loop.run_until_complete(scrape_and_send_stickers())
+if __name__=='__main__':
+    with client:
+        client.loop.run_until_complete(scrape_and_send_stickers())
